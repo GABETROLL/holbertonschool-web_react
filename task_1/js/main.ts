b15a73ffcb9dfd4bc85722e21883f5ebe04b8674
GABETROLL <gabriel7icplhappy@gmail.com>
2024-01-03 14:00:23 -0400

    Add the Student stuff to task 1

--- task_1/js/main.ts
+++ task_1/js/main.ts
@@ -14,10 +14,34 @@ export interface Directors extends Teacher {
 export interface printTeacherFunction {
   (firstName: string, lastName: string): string;
 }
-
 export const printTeacher: printTeacherFunction = function (firstName: string, lastName: string): string {
   return `${firstName[0]}. ${lastName}`;
 };
+
+export interface StudentClassInterface {
+  displayName(): string;
+  workOnHomework(): string;
+}
+export interface StudentConstructor {
+  new(firstName: string, lastName: string): StudentClassInterface;
+}
+export const StudentClass: StudentConstructor = class StudentClass implements StudentClassInterface {
+  firstName: string;
+  lastName: string;
+
+  constructor(firstName: string, lastName: string) {
+    this.firstName = firstName;
+    this.lastName = lastName
+  }
+
+  workOnHomework(): string {
+    return 'Currently working';
+  }
+
+  displayName(): string {
+    return this.firstName;
+  }
+}
 
 // TESTS
 
