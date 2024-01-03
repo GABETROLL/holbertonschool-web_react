8c638adf139ed22988f596231875be513c2a514e
GABETROLL <gabriel7icplhappy@gmail.com>
2024-01-03 11:57:52 -0400

    Add task 1

---
+++ task_1/js/test.ts
@@ -0,0 +1,23 @@
+import { Teacher } from './main';
+
+const teacher: Teacher = {
+  firstName: 'A',
+  lastName: 'B',
+  location: 'Puerto Rico',
+  fullTimeEmployee: false,
+  contract: false,
+};
+
+console.log(teacher);
+
+/*
+Should print:
+
+{
+  firstName: 'A',
+  lastName: 'B',
+  location: 'Puerto Rico',
+  fullTimeEmployee: false,
+  contract: false,
+}
+*/
