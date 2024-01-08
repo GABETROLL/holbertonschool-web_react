7e053257c8eb375017a4c860f636783988ebf0e0
GABETROLL <gabriel7icplhappy@gmail.com>
2024-01-07 22:49:29 -0400

    Re-build

--- Webpack/task_2/js/dashboard_main.js
+++ Webpack/task_2/js/dashboard_main.js
@@ -2,7 +2,7 @@ import $ from 'jquery';
 import _ from 'lodash';
 import '../css/main.css';
 
-$('body').append('<span id="logo"></span>');
+$('body').append('<div id="logo"></div>');
 
 $('body').append('<p>Holberton Dashboard</p>');
 $('body').append('<p>Dashboard data for the students</p>');
