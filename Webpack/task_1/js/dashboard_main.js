7a506b5baf1297f384ae3c863e69843d9dd07114
GABETROLL <gabriel7icplhappy@gmail.com>
2024-01-08 13:21:51 -0400

    Correct clicks counter

--- Webpack/task_1/js/dashboard_main.js
+++ Webpack/task_1/js/dashboard_main.js
@@ -7,9 +7,11 @@ $('body').append('<button>Click here to get started</button>');
 $('body').append("<p id='count'></p>");
 $('body').append('<p>Copyright - Holberton School</p>');
 
+let count = 0;
+
 function updateCounter() {
-  const count = ($('#count').text() || '').split(' ')[0] || 0;
-  $('#count').text(`${count + 1} clicks on the button`);
+  count++;
+  $('#count').text(`${count} clicks on the button`);
 }
 
-$('#count').on('click', _.debounce(updateCounter, 500));
+$('button').on('click', _.debounce(updateCounter, 500));
