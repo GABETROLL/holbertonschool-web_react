e39cb8fd9106a9aa0b3f93cc623d4cbc6dc987f7
GABETROLL <gabriel7icplhappy@gmail.com>
2024-01-07 20:22:29 -0400

    Make the counter clicking work, and make it bold

--- Webpack/task_2/js/dashboard_main.js
+++ Webpack/task_2/js/dashboard_main.js
@@ -2,6 +2,8 @@ import $ from 'jquery';
 import _ from 'lodash';
 import '../css/main.css';
 
+$('body').append('<span id="logo"></span>');
+
 $('body').append('<p>Holberton Dashboard</p>');
 $('body').append('<p>Dashboard data for the students</p>');
 $('body').append('<button>Click here to get started</button>');
@@ -9,8 +11,8 @@ $('body').append("<p id='count'></p>");
 $('body').append('<p>Copyright - Holberton School</p>');
 
 function updateCounter() {
-  const count = ($('#count').text() || '').split(' ')[0] || 0;
+  const count = parseInt(($('#count').text() || '').split(' ')[0] || '0');
   $('#count').text(`${count + 1} clicks on the button`);
 }
 
-$('#count').on('click', _.debounce(updateCounter, 500));
+$('button').on('click', _.debounce(updateCounter, 500));
