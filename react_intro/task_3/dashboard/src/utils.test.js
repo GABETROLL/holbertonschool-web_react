a412e28ef9de084dbdc5d53e799409575c158331
GABETROLL <gabriel7icplhappy@gmail.com>
2024-01-12 16:50:53 -0400

    Use jest's expect and re-write App.test.js and Notifications.test.js

--- react_intro/task_3/dashboard/src/utils.test.js
+++ react_intro/task_3/dashboard/src/utils.test.js
@@ -1,22 +1,21 @@
 import { getFullYear, getFooterCopy, getLatestNotification } from './utils';
-import assert from 'assert';
 
 describe('getFullYear', () => {
   it('returns the current year', () => {
     // ?
-    assert.equal(getFullYear(), (new Date()).getFullYear());
+    expect(getFullYear()).toBe((new Date()).getFullYear());
   });
 });
 
 describe('getFooterCopy', () => {
   it('returns the correct string when its `index` argument is true or false', () => {
-    assert.equal(getFooterCopy(false), 'Holberton School main dashboard');
-    assert.equal(getFooterCopy(true), 'Holberton School');
+    expect(getFooterCopy(false)).toEqual('Holberton School main dashboard');
+    expect(getFooterCopy(true)).toEqual('Holberton School');
   });
 });
 
 describe('getLatestNotification', () => {
   it('returns: "<strong>Urgent requirement</strong> - complete by EOD"', () => {
-    assert.equal(getLatestNotification(), '<strong>Urgent requirement</strong> - complete by EOD');
+    expect(getLatestNotification()).toEqual('<strong>Urgent requirement</strong> - complete by EOD');
   });
 });
