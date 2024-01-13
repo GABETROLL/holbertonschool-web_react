d2a93d663f38b92e8b04198a1612e6436b8a3c2a
GABETROLL <gabriel7icplhappy@gmail.com>
2024-01-13 17:07:50 -0400

    Try to configue babel-loader for JSX

--- react_intro/task_5/dashboard/config/webpack.config.js
+++ react_intro/task_5/dashboard/config/webpack.config.js
@@ -30,7 +30,19 @@ module.exports = {
             },
           },
         ],
-      }
+      },
+      {
+        test: /\.(?:js|mjs|cjs)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              ['@babel/preset-env', { targets: "defaults" }],
+            ],
+          },
+        },
+      },
     ],
   },
 };
