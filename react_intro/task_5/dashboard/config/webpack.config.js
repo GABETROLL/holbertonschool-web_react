04c626b17ba4e2cdb7ce4ec01f5198a021a5d3e2
GABETROLL <gabriel7icplhappy@gmail.com>
2024-01-12 21:44:44 -0400

    fix typo

--- react_intro/task_5/dashboard/config/webpack.config.js
+++ react_intro/task_5/dashboard/config/webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
   entry: './src/index.js',
-  devtool: 'inline-cheap-source-map',
+  devtool: 'inline-source-map',
   mode: 'development',
   devServer: {
     static: './dist',
