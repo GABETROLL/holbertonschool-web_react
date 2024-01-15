c1438c526e4e589a526c123f1d9493326c1e2f6b
GABETROLL <gabriel7icplhappy@gmail.com>
2024-01-15 15:22:01 -0400

    Add new tests

--- react_props/task_1/dashboard/src/App/App.test.js
+++ react_props/task_1/dashboard/src/App/App.test.js
@@ -1,6 +1,27 @@
 import { shallow } from 'enzyme';
 import App from './App';
+import Notifications from '../Notifications/Notifications';
 
 describe('<App />', () => {
-  /* ADD TEST FOR CRASH */
+  let wrapper;
+  beforeAll(() => {
+    wrapper = shallow(<App />);
+  });
+
+  it('renders without crashing', () => {
+    expect(wrapper.exists()).toBe(true);
+  });
+
+  it('contains one Notifications component', () => {
+    expect(wrapper.find(<Notifications />)).toHaveLength(1);
+  });
+  it('contains one Header component', () => {
+    expect(wrapper.find(<Header />)).toHaveLength(1);
+  });
+  it('contains one Login component', () => {
+    expect(wrapper.find(<Login />)).toHaveLength(1);
+  });
+  it('contains one Footer component', () => {
+    expect(wrapper.find(<Footer />)).toHaveLength(1);
+  });
 });
