baa979bada3d20126f4210c008bd7601bf3331f4
GABETROLL <gabriel7icplhappy@gmail.com>
2024-01-18 15:13:40 -0400

    Remove autocomplete issue

--- react_props/task_4/dashboard/src/Login/Login.js
+++ react_props/task_4/dashboard/src/Login/Login.js
@@ -7,7 +7,7 @@ export default function Login() {
       <p>Login to access the full dashboard</p>
       <div className="login">
         <label htmlFor="email">Email:</label>
-        <input id="email" name="email" type="email"/>
+        <input id="email" name="email" type="email" autoComplete="true"/>
         <label htmlFor="password">Password:</label>
         <input id="password" name="password" type="text"/>
         <button>OK</button>
