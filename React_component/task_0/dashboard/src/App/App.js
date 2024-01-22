2435f71c4b7a1311c542e7bbf889fc91e72bd49a
GABETROLL <gabriel7icplhappy@gmail.com>
2024-01-22 14:00:34 -0400

    Remove whitespaces from task_0/

--- React_component/task_0/dashboard/src/App/App.js
+++ React_component/task_0/dashboard/src/App/App.js
@@ -15,13 +15,13 @@ class App extends React.Component {
       { id: 2, type: 'urgent', value: 'New resume available' },
       { id: 3, type: 'urgent', html: {__html: getLatestNotification()} },
     ];
-  
+
     const listCourses = [
       { id: 1, name: 'ES6', credit: 60 },
       { id: 2, name: 'Webpack', credit: 20 },
       { id: 3, name: 'React', credit: 40 },
     ];
-  
+
     return (
       <>
         <Notifications listNotifications={listNotifications} />
