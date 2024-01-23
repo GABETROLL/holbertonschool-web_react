2b6229c38a37a894a0e915ef7c5e0d23d9447154
GABETROLL <gabriel7icplhappy@gmail.com>
2024-01-23 14:52:58 -0400

    Make the HOC name test pass

--- React_component/task_4/dashboard/src/Login/Login.js
+++ React_component/task_4/dashboard/src/Login/Login.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './Login.css';
 
-export default function Login() {
+function Login() {
   return (
     <>
       <p>Login to access the full dashboard</p>
@@ -15,3 +15,5 @@ export default function Login() {
     </>
   );
 }
+Login.displayName = 'Login';
+export default Login;
