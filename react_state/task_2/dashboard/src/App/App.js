eb83b2cb2ac1d6cfce3455b3853894ef00b291ee
GABETROLL <gabriel7icplhappy@gmail.com>
2024-02-02 12:28:33 -0400

    Copy difference to next tasks

--- react_state/task_2/dashboard/src/App/App.js
+++ react_state/task_2/dashboard/src/App/App.js
@@ -101,9 +101,11 @@ export default class App extends React.Component {
       { id: 3, name: 'React', credit: 40 },
     ];
 
+    const { displayDrawer } = this.state;
+
     return (
       <AppContext.Provider value={this.state.value}>
-        <Notifications listNotifications={listNotifications} displayDrawer={this.state.displayDrawer} handleDisplayDrawer={this.handleDisplayDrawer} handleHideDrawer={this.handleHideDrawer} />
+        <Notifications listNotifications={listNotifications} displayDrawer={displayDrawer} handleDisplayDrawer={this.handleDisplayDrawer} handleHideDrawer={this.handleHideDrawer} />
         <div className={css(styles.app)}>
           <Header />
           <div className={css(styles.body)}>
--- react_state/task_2/dashboard/src/Notifications/Notifications.js
+++ react_state/task_2/dashboard/src/Notifications/Notifications.js
@@ -114,9 +114,11 @@ class Notifications extends React.Component {
   }
 
   render() {
+    const { handleDisplayDrawer, handleHideDrawer } = this.props;
+
     const menuItemElement = (
       <div className={css(styles.menuItem, this.props.displayDrawer && styles.hiddenMenuItem)}>
-        <p className={css(styles.menuItemP)} onClick={this.props.handleDisplayDrawer}>Your notifications</p>
+        <p className={css(styles.menuItemP)} onClick={handleDisplayDrawer}>Your notifications</p>
       </div>
     );
 
@@ -134,7 +136,7 @@ class Notifications extends React.Component {
 
     const notificationsDrawer = (
       <div className={css(this.props.displayDrawer ? styles.NotificationsDrawerOpen : undefined)}>
-        <button onClick={this.props.handleHideDrawer} style={{
+        <button onClick={handleHideDrawer} style={{
           float: 'right',
           paddingTop: 18,
           paddingRight: 15,
