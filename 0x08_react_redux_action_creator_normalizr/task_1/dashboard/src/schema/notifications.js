10c69e61b877eec3ced0c5bd89de6aeda7aea347
GABETROLL <gabriel7icplhappy@gmail.com>
2024-02-12 15:12:46 -0400

    normalize the notifications data

--- 0x08_react_redux_action_creator_normalizr/task_1/dashboard/src/schema/notifications.js
+++ 0x08_react_redux_action_creator_normalizr/task_1/dashboard/src/schema/notifications.js
@@ -1,4 +1,13 @@
 import * as notifications from '../notifications.json';
+import { normalize, schema } from 'normalizr';
+
+const user = new schema.Entity('users');
+const message = new schema.Entity('messages', { idAttribute: 'guid' });
+const notification = new schema.Entity('notifications', {
+  author: user,
+  context: message,
+});
+export const notifications = normalize(notifications, notification);
 
 export function getAllNotificationsByUser(userId) {
   return notifications
