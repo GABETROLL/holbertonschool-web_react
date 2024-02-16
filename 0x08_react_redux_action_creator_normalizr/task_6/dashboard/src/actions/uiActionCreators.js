fb1b82e9c1750425c423f9ab3d2ecff4383565b2
GABETROLL <gabriel7icplhappy@gmail.com>
2024-02-16 16:23:45 -0400

    Use redux store to bind functions in task_6/

--- 0x08_react_redux_action_creator_normalizr/task_6/dashboard/src/actions/uiActionCreators.js
+++ 0x08_react_redux_action_creator_normalizr/task_6/dashboard/src/actions/uiActionCreators.js
@@ -1,7 +1,10 @@
 import { bindActionCreators} from 'redux';
-import { LOGIN, LOGOUT, DISPLAY_NOTIFICATION_DRAWER, HIDE_NOTIFICATION_DRAWER } from './uiActionTypes';
+import store from './store';
+import { LOGIN, LOGOUT, LOGIN_SUCCESS, LOGIN_FAILURE, DISPLAY_NOTIFICATION_DRAWER, HIDE_NOTIFICATION_DRAWER } from './uiActionTypes';
 
-export const login = bindActionCreators((email, password) => ({ type: LOGIN, user: { email, password } }));
-export const logout = bindActionCreators(() => ({ type: LOGOUT }));
-export const displayNotificationDrawer = bindActionCreators(() => ({ type: DISPLAY_NOTIFICATION_DRAWER }));
-export const hideNotificationDrawer = bindActionCreators(() => ({ type: HIDE_NOTIFICATION_DRAWER }));
+export const login = bindActionCreators((email, password) => ({ type: LOGIN, user: { email, password } }), store.dispatch);
+export const logout = bindActionCreators(() => ({ type: LOGOUT }), store.dispatch);
+export const displayNotificationDrawer = bindActionCreators(() => ({ type: DISPLAY_NOTIFICATION_DRAWER }), store.dispatch);
+export const hideNotificationDrawer = bindActionCreators(() => ({ type: HIDE_NOTIFICATION_DRAWER }), store.dispatch);
+export const loginSuccess = bindActionCreators(() => ({ type: LOGIN_SUCCESS }), store.dispatch);
+export const loginFailure = bindActionCreators(() => ({ type: LOGIN_FAILURE }), store.dispatch);
