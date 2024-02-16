7b82d6ef576e5b786a3e69f6699f17c6fc1d8a81
GABETROLL <gabriel7icplhappy@gmail.com>
2024-02-16 13:01:59 -0400

    Correct task 1 to use the auto-normalized JSON

--- 0x08_react_redux_action_creator_normalizr/task_1/dashboard/src/schema/notifications.js
+++ 0x08_react_redux_action_creator_normalizr/task_1/dashboard/src/schema/notifications.js
@@ -1,16 +1,17 @@
-import * as notifications_json_obj from '../notifications.json';
+import * as notifications_json_obj from '../../notifications.json';
 import { normalize, schema } from 'normalizr';
 
 const user = new schema.Entity('users');
-const message = new schema.Entity('messages', { idAttribute: 'guid' });
+const message = new schema.Entity('messages', {}, { idAttribute: 'guid' });
 const notification = new schema.Entity('notifications', {
   author: user,
   context: message,
 });
-export const notifications = normalize(notifications_json_obj, [notification]);
+
+export const notifications = normalize(notifications_json_obj.default, [notification]);
 
 export function getAllNotificationsByUser(userId) {
-  return notifications
-    .filter(notification => notification.author.id === userId)
+  return notifications_json_obj.default
+    .filter(notification => notification.author && notification.author.id === userId)
     .map(notification => notification.context);
 }
